7bb87213185940a33080e491ac359d088b8cab4c
Annie Huang <annie.hn@gmail.com>
2018-12-30 16:42:21 +1100

    Add contactmanager routing in app-routing.module and made it as default. Imported 3 modules in contactmanager.module: MaterialModule, FormsModule, FlexLayoutModule.

--- src/app/contactmanager/contactmanager.module.ts
+++ src/app/contactmanager/contactmanager.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import {MaterialModule} from '../shared/material.module';
+import {FormsModule} from '@angular/forms';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
 import { ContactmanagerAppComponent } from './contactmanager-app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
@@ -8,7 +13,10 @@ import { SidenavComponent } from './components/sidenav/sidenav.component';
 @NgModule({
   declarations: [ContactmanagerAppComponent, ToolbarComponent, MainContentComponent, SidenavComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    FormsModule,
+    FlexLayoutModule
   ]
 })
 export class ContactmanagerModule { }
