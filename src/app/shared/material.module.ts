1f5e55b100d77c4a9bd0f86f96c4b5b184d84341
Annie Huang <annie.hn@gmail.com>
2018-12-30 14:27:29 +1100

    Add angularMaterial into a separate module.

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatButtonModule,
+    MatCheckboxModule
+  ]
+})
+export class MaterialModule { }
