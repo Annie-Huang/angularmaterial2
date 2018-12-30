7cb979efacbee3cf92eba8581eef46b108965349
Annie Huang <annie.hn@gmail.com>
2018-12-30 23:10:45 +1100

    Checked if we can retrieve users from sidenav.component. Success

--- src/app/contactmanager/components/sidenav/sidenav.component.ts
+++ src/app/contactmanager/components/sidenav/sidenav.component.ts
@@ -1,4 +1,7 @@
 import {Component, NgZone, OnInit} from '@angular/core';
+import {User} from '../../models/user';
+import {UserService} from '../../services/user.service';
+import {Observable} from 'rxjs';
 
 const SMALL_WIDTH_BREAKPOINT = 720;
 
@@ -12,15 +15,23 @@ export class SidenavComponent implements OnInit {
   private mediaMatcher: MediaQueryList =
     matchMedia(`(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`);
 
+  users: Observable<User[]>;
+
   // // Don't seem to need this. Looks like angular material or angular have fixed this in version 7,
   // constructor(zone: NgZone) {
   //   this.mediaMatcher.addListener(mql =>
   //     zone.run(() => this.mediaMatcher = mql));
   // }
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.users = this.userService.users;
+    this.userService.loadAll();
+
+    this.users.subscribe(data => {
+      console.log(data);
+    });
   }
 
   isScreenSmall(): boolean {
