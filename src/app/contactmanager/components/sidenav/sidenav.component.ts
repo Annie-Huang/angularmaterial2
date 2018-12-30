4d05231605dfa59ec64bacb4817354d66b41d74c
Annie Huang <annie.hn@gmail.com>
2018-12-30 21:24:57 +1100

    Changed opened and mode to link to isScreenSmall()

--- src/app/contactmanager/components/sidenav/sidenav.component.ts
+++ src/app/contactmanager/components/sidenav/sidenav.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, NgZone, OnInit} from '@angular/core';
+
+const SMALL_WIDTH_BREAKPOINT = 720;
 
 @Component({
   selector: 'app-sidenav',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidenavComponent implements OnInit {
 
+  private mediaMatcher: MediaQueryList =
+    matchMedia(`(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`);
+
+  // // Don't seem to need this. Looks like angular material or angular have fixed this in version 7,
+  // constructor(zone: NgZone) {
+  //   this.mediaMatcher.addListener(mql =>
+  //     zone.run(() => this.mediaMatcher = mql));
+  // }
+  
   constructor() { }
 
   ngOnInit() {
   }
 
+  isScreenSmall(): boolean {
+    return this.mediaMatcher.matches;
+  }
 }
